f929c80cf08abe9aebfc4a841d915d590d590bb8
souravkairy <48748661+souravkairy@users.noreply.github.com>
2022-05-24 15:29:04 +0600

    react hook form added

--- client/src/Routes/AppRoutes.js
+++ client/src/Routes/AppRoutes.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import AddBlogPage from '../pages/AddBlogPage';
 import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
+import RegistrationPage from '../pages/RegistrationPage';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="add-blog" element={<AddBlogPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegistrationPage />} />
         </Routes>
     )
 }
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useForm } from "react-hook-form";
+
+const LoginPage = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+
+    return (
+        <div className="mx-5 my-5 sm:rounded-lg ">
+            <div className="max-w-md mx-auto bg-gray-300 p-6">
+                <div className="">
+                    <div className='mb-6'>
+                        <h3 className="text-4xl leading-6 font-medium text-gray-900">Login</h3>
+                    </div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="space-y-4">
+                            <div className="">
+                                <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                                    Email
+                                </label>
+                                <input
+                                    {...register("email", { required: true })}
+                                    className="mt-1 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-sm py-2 px-2 outline-hidden"
+                                />
+                                <p className='text-xs text-red-500'>{errors.email && <span>This field is required</span>}</p>
+                            </div>
+
+                            <div className="">
+                                <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                                    Password
+                                </label>
+                                <input
+                                    {...register("password", { required: true })}
+                                    name="password"
+                                    className="mt-1 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-sm py-2 px-2"
+                                />
+                                <p className='text-xs text-red-500'>{errors.password && <span>This field is required</span>}</p>
+                            </div>
+                            <div className="flex justify-center">
+                                <button
+                                    type="submit"
+                                    className="w-full py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-sm text-white bg-indigo-600 hover:bg-indigo-700 "
+                                >
+                                    login
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage
